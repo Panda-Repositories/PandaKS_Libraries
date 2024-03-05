6f0ab2b827b632e673c9d0b6b53ff0d660b9de70
SkieHacker <40538401+SkieAdmin@users.noreply.github.com>
2024-03-05 08:17:06 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,13 +1,10 @@
-local APILoader = {}
-
 local http_service = cloneref(game:GetService("HttpService"))
 local json = game:HttpGet("https://raw.githubusercontent.com/Panda-Repositories/PandaKS_Libraries/main/LibraryConfig/ApiServiceConfiguration.json") 
 local configuration = http_service:JSONDecode(json)
 local mainUrl = configuration.Server_Domain.Main_URL
+local PandaAuth
 
-function APILoader:RunLibraries(version)
-    local PandaAuth
-
+function RunLibraries(version)
     if version == "v3" then
         PandaAuth = loadstring(game:HttpGet(configuration.API_Configuration.v3_Library))()
     elseif version == "v4" then
@@ -20,14 +17,15 @@ function APILoader:RunLibraries(version)
 end
 
 if getgenv().PelicanKeySys_Version == nil then
-  return APILoader:RunLibraries("v2")
+  return RunLibraries("v2")
 elseif getgenv().PelicanKeySys_Version == "v3" then
-  return APILoader:RunLibraries("v3")
+  return RunLibraries("v3")
 elseif getgenv().PelicanKeySys_Version == "v4" then
-  return APILoader:RunLibraries("v4")
+  return RunLibraries("v4")
 else
-  return APILoader:RunLibraries("v2")
+  return RunLibraries("v2")
 end
 
+print("Successfully Loaded...")
 
-return APILoader
+return PandaAuth
