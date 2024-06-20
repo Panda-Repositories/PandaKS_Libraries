4ea68010103290a11524990bfee650118ed5c21a
SkieHacker <kerlemmorsti@gmail.com>
2024-06-20 11:56:52 +0800

    Update VanguardCore_E.lua

--- library/VanguardCore_E.lua
+++ library/VanguardCore_E.lua
@@ -1,6 +1,7 @@
-local VanguardCore = {}
+-- VigenereCipher Module
+local VigenereCipher = {}
 
-local function stringToBytes(str)
+function VigenereCipher.stringToBytes(str)
     local bytes = {}
     for i = 1, #str do
         table.insert(bytes, string.byte(str, i))
@@ -8,7 +9,7 @@ local function stringToBytes(str)
     return bytes
 end
 
-local function bytesToString(bytes)
+function VigenereCipher.bytesToString(bytes)
     local chars = {}
     for i = 1, #bytes do
         table.insert(chars, string.char(bytes[i]))
@@ -16,7 +17,7 @@ local function bytesToString(bytes)
     return table.concat(chars)
 end
 
-local function bytesToHex(bytes)
+function VigenereCipher.bytesToHex(bytes)
     local hex = {}
     for i = 1, #bytes do
         table.insert(hex, string.format("%02x", bytes[i]))
@@ -24,7 +25,7 @@ local function bytesToHex(bytes)
     return table.concat(hex)
 end
 
-local function hexToBytes(hex)
+function VigenereCipher.hexToBytes(hex)
     local bytes = {}
     for i = 1, #hex, 2 do
         table.insert(bytes, tonumber(hex:sub(i, i+1), 16))
@@ -32,9 +33,9 @@ local function hexToBytes(hex)
     return bytes
 end
 
-local function vigenereEncryptBytes(bytes, key)
+function VigenereCipher.vigenereEncryptBytes(bytes, key)
     key = key:upper()
-    local keyBytes = stringToBytes(key)
+    local keyBytes = VigenereCipher.stringToBytes(key)
     local encryptedBytes = {}
     for i = 1, #bytes do
         local byte = bytes[i]
@@ -44,9 +45,9 @@ local function vigenereEncryptBytes(bytes, key)
     return encryptedBytes
 end
 
-local function vigenereDecryptBytes(bytes, key)
+function VigenereCipher.vigenereDecryptBytes(bytes, key)
     key = key:upper()
-    local keyBytes = stringToBytes(key)
+    local keyBytes = VigenereCipher.stringToBytes(key)
     local decryptedBytes = {}
     for i = 1, #bytes do
         local byte = bytes[i]
@@ -56,19 +57,4 @@ local function vigenereDecryptBytes(bytes, key)
     return decryptedBytes
 end
 
-
-function VanguardCore:VTec_Encrypt(plaintext, authentication_key)
-    local plaintextBytes = stringToBytes(plaintext)
-    local encryptedBytes = vigenereEncryptBytes(plaintextBytes, authentication_key)
-    local encryptedText = bytesToHex(encryptedBytes)
-    return encryptedText;
-end
-
-function VanguardCore:VTec_Decrypt(encryptedText, authentication_key)
-    local decryptedBytes = vigenereDecryptBytes(hexToBytes(encryptedText), authentication_key)
-    local finalDecryptedText = bytesToString(decryptedBytes)
-    return finalDecryptedText;
-end
-
-
-return VanguardCore
+return VigenereCipher
